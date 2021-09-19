fdd1f150e224ff7987ad510196d8f38d945146c5
Ricardo Maginador <ricardo.maginador@wildlifestudios.com>
2021-09-19 17:13:30 -0300

    implement basic login with Playfab

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Player : MonoBehaviour
+    {
+        public static Profile CurrentProfile;
+        public static void CreateNewPlayer()
+        {
+            CurrentProfile = new Profile(SystemInfo.deviceUniqueIdentifier);
+        }
+    }
+
+    public class Profile
+    {
+        public Profile(string id)
+        {
+            ID = id;
+        }
+
+        private static string ID { get; set; }
+
+        public static string GetID()
+        {
+            return ID;
+        }
+    }
---
+++ Assets/Scripts/Playfab/PlayfabManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayfabManager : MonoBehaviour
+{
+
+
+    public Text debug;
+    public static PlayfabManager Instance;
+
+    public void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }   
+    }
+
+    public void PlayerLogin()
+    {
+        Player.CreateNewPlayer();
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = Profile.GetID(),
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnCustomLoginSuccess, OnError);
+
+    }
+
+    public void LoginWithGooglePlay()
+    {
+        //TODO:: Develop Google Play login
+        //var request = new LoginWithGoogleAccountRequest();
+        //PlayFabClientAPI.LoginWithGoogleAccount(request, OnGoogleLogin, OnError);
+    }
+
+    private static void OnCustomLoginSuccess(LoginResult loginResult)
+    {
+        Debug.Log("Logged as " + loginResult.PlayFabId);
+        Instance.debug.text = "Session Ticket : " + loginResult.SessionTicket;
+        Instance.debug.text += "\nSession EntityToken : " + loginResult.EntityToken.EntityToken;
+        Instance.debug.text += "\nSession Id : " + loginResult.PlayFabId;
+    }
+    private static void OnGoogleLogin(LoginResult loginResult)
+    {
+        //TODO:: Develop Google Play login
+    }
+
+    private static void OnError(PlayFabError playFabError)
+    {
+        Debug.LogError("Request got error : " + playFabError.Error);
+    }
+}
