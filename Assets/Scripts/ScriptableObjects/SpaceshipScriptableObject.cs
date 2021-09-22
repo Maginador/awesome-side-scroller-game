e53003d72c459320e8df8b9929f0bb8711048be6
Ricardo Maginador <ricardo.maginador@wildlifestudios.com>
2021-09-22 01:27:53 -0300

    creates scriptable object structure for spaceships

---
+++ Assets/Scripts/ScriptableObjects/SpaceshipScriptableObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "Spaceship", menuName = "ScriptableObjects/Space Ship", order = 1)]
+    public class SpaceshipScriptableObject : ScriptableObject
+    {
+        public string spaceshipName;
+        public int healthPoints;
+        public int speed;
+        public int energy;
+        public float shootRate;
+        public GameObject destructionVFX;
+        public GameObject bullet;
+    }
+}
