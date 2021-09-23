a8ec4eac417f3079613aaea89d946c5042b6cc9a
Ricardo Maginador <ricardo.maginador@wildlifestudios.com>
2021-09-22 23:27:06 -0300

    improve UI, add top menu and new assets, add actions references to the buttons

--- Assets/Scripts/GUI/MainGUI.cs
+++ Assets/Scripts/GUI/MainGUI.cs
@@ -1,18 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class MainGUI : MonoBehaviour
+namespace GUI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MainGUI : MonoBehaviour
     {
-        
-    }
+        //GUI Screens
+        [SerializeField] private GameObject mainScreen;
+        [SerializeField] private GameObject progressScreen;
+        [SerializeField] private GameObject endlessScreen;
+        [SerializeField] private GameObject storeScreen;
+        [SerializeField] private GameObject upgradeScreen;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private GameObject soundDisabled;
+        public enum Screens { Main, Progress, Endless, Store, Upgrade}
+
+        public Screens currentScreen;
+
+        public void Awake()
+        {
+            currentScreen = Screens.Main;
+        }
+
+        public void BackToMainScreen()
+        {
+            mainScreen.SetActive(true);
+            switch (currentScreen)
+            {
+                case Screens.Progress:
+                    progressScreen.SetActive(false);
+                    break;
+                case Screens.Endless:
+                    endlessScreen.SetActive(false);
+                    break;
+                case Screens.Store:
+                    storeScreen.SetActive(false);
+                    break;
+                case Screens.Upgrade:
+                    upgradeScreen.SetActive(false);
+                    break;
+            }
+        }
+        public void OpenProgressScreen()
+        {
+            mainScreen.SetActive(false);
+            progressScreen.SetActive(true);
+        }
+
+        public void OpenEndlessScreen()
+        {
+            mainScreen.SetActive(false);
+            endlessScreen.SetActive(true);
+        }
+
+        public void ToggleSound()
+        {
+            soundDisabled.SetActive(!soundDisabled.activeSelf);
+        }
+
+        public void Quit()
+        {
+            //TODO: create safe quit screen (verify if user really wants to quit)
+            Application.Quit();
+        }
+
+        public void OpenStore()
+        {
+            mainScreen.SetActive(false);
+            storeScreen.SetActive(true);
+        }
+
+        public void OpenUpgrade()
+        {
+            mainScreen.SetActive(false);
+            upgradeScreen.SetActive(true);
+        }
     }
 }
