5ec059d3855149a309041bd2463da424f0b9d932
Ricardo Maginador <ricardo.maginador@wildlifestudios.com>
2021-09-26 01:32:05 -0300

    Adds enemy movement using speed parameter from scriptableobject

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Enemy : Spaceship
 {
-    
+    [SerializeField] private Rigidbody rigidBody;
+    private void Move()
+    {
+        if (spaceshipData.speed > 0)
+        {
+            transform.Translate(Vector3.up * (spaceshipData.speed * Time.deltaTime),Space.Self);
+        }
+    }
+
+    private new void Update()
+    {
+        base.Update();
+
+        Move();
+    }
 }
